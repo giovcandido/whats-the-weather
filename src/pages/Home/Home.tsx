00440efde5858303e1b4bbcc03efc6b75f3465ef
giovcandido <giovcandido@outlook.com>
2020-10-08 15:21:19 -0300

    Add logo to home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import logo from '../../assets/logo.svg';
 import styles from './Home.module.sass';
 
 const Home: React.FC = () => {
   return(
     <div className={styles.pageContainer}>
       <header className={styles.pageHeader}>
+        <img className={styles.logo} src={logo} alt="logo with clouds" />
         <h3>What's the weather?</h3>
       </header>
       <section className={styles.pageSearch}>
@@ -19,6 +21,7 @@ const Home: React.FC = () => {
         <article className={styles.card}>
           <header className={styles.cardHeader}>
             <span>London</span>
+            <span>GB</span>
           </header>
           <div className={styles.cardMain}>
             <img src="https://cdn0.iconfinder.com/data/icons/weather-forecast-17/128/forecast-weather_rain-heavy-drizzle-512.png" />
