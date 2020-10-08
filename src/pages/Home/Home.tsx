df0f0faaeeee06843c442fc50de667d833621dd3
giovcandido <giovcandido@outlook.com>
2020-10-08 11:09:00 -0300

    Restructure home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 
 const Dashboard: React.FC = () => {
   return(
-    <>
+    <div>
       <header>
+        <h1>What's the weather?</h1>
       </header>
       <section>
-        <h1>Home</h1>
+        <form>
+          <input type="text" placeholder="Enter a city name" />
+          <button type="submit">Search</button>
+        </form>
+      </section>
+      <section>
+        <h2>City cards here:</h2>
       </section>
       <footer>
         <p>Made by <a href="https://github.com/giovcandido" rel="noopener noreferrer" target="_blank">Giovani Candido</a></p>
       </footer>
-    </>
+    </div>
   );
 };
 
