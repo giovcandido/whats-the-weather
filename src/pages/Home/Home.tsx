c492530e64c33e87c564d6f55a73d0a5ac09053f
giovcandido <giovcandido@outlook.com>
2020-10-08 12:35:33 -0300

    Change home cards class

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,16 +9,14 @@ const Home: React.FC = () => {
         <h3>What's the weather?</h3>
       </header>
       <section className={styles.pageSearch}>
-        <h2>Get weather information for any city you want</h2>
+        <h1>Get weather information for any city you want</h1>
         <form>
-          <input type="text" placeholder="Enter a city name" />
+          <input type="text" placeholder="Enter the city name" />
           <button type="submit">Search</button>
         </form>
       </section>
-      <section className={styles.pageContent}>
-        <section className={styles.pageCards}>
-          <h2>City cards here:</h2>
-        </section>
+      <section className={styles.pageCards}>
+        <h2>City cards here:</h2>
       </section>
       <footer className={styles.pageFooter}>
         <p>Made by <a href="https://github.com/giovcandido" rel="noopener noreferrer" target="_blank">Giovani Candido</a></p>
