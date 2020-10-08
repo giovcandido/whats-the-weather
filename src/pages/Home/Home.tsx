32e16ed38d3f33ba2702ec6605b2e17d5660b126
giovcandido <giovcandido@outlook.com>
2020-10-08 12:08:05 -0300

    Set basic layout for home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 
+import styles from './Home.module.sass';
+
 const Home: React.FC = () => {
   return(
-    <div>
-      <header>
-        <h1>What's the weather?</h1>
+    <div className={styles.pageContainer}>
+      <header className={styles.pageHeader}>
+        <h3>What's the weather?</h3>
       </header>
-      <section>
+      <section className={styles.pageSearch}>
+        <h2>Get weather information for any city you want</h2>
         <form>
           <input type="text" placeholder="Enter a city name" />
           <button type="submit">Search</button>
         </form>
       </section>
-      <section>
-        <h2>City cards here:</h2>
+      <section className={styles.pageContent}>
+        <section className={styles.pageCards}>
+          <h2>City cards here:</h2>
+        </section>
       </section>
-      <footer>
+      <footer className={styles.pageFooter}>
         <p>Made by <a href="https://github.com/giovcandido" rel="noopener noreferrer" target="_blank">Giovani Candido</a></p>
       </footer>
     </div>
