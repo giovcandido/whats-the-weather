3afd979ec79e34ea9b456f9ce9bbeb14c355fbc9
giovcandido <giovcandido@outlook.com>
2020-10-11 11:49:04 -0300

    Updates

---
+++ precache-manifest.2e5a30a73254e67b636dcbd241de912a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f2260224a5c4158a65ee263c19a2ee",
+    "url": "/giovcandido/whats-the-weather/index.html"
+  },
+  {
+    "revision": "b860d2f081d67e612bb9",
+    "url": "/giovcandido/whats-the-weather/static/css/main.a286953c.chunk.css"
+  },
+  {
+    "revision": "669df437ef187b76edfd",
+    "url": "/giovcandido/whats-the-weather/static/js/2.dffde90d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/giovcandido/whats-the-weather/static/js/2.dffde90d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b860d2f081d67e612bb9",
+    "url": "/giovcandido/whats-the-weather/static/js/main.11fd239d.chunk.js"
+  },
+  {
+    "revision": "2d9daad1af8fd7a26d41",
+    "url": "/giovcandido/whats-the-weather/static/js/runtime-main.cd139909.js"
+  },
+  {
+    "revision": "d488f7c055891ad5d19adad09672ca88",
+    "url": "/giovcandido/whats-the-weather/static/media/logo.d488f7c0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1bec74e0637b17f84aedf55f43fa6912.js"
+  "/giovcandido/whats-the-weather/precache-manifest.2e5a30a73254e67b636dcbd241de912a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/giovcandido/whats-the-weather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
