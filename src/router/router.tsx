4b0242d8424267ac3372d2b3c7cb1554ff40d022
giovcandido <giovcandido@outlook.com>
2020-10-11 18:08:18 -0300

    Isolate all router related components

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home/Home';
 import City from '../pages/City/City';
 
 const Router: React.FC = () => {
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/city" component={City} />
-    </Switch>
+    <BrowserRouter basename="whats-the-weather">
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/city" component={City} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
