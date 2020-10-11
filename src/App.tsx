a025ef4cd4140469b1b0884957dc0f92abd4ca8b
giovcandido <giovcandido@outlook.com>
2020-10-11 18:08:30 -0300

    Isolate all router related components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 
 import Router from './router/router';
 
 import './App.sass';
 
 const App: React.FC = () => {
-  return (
-    <BrowserRouter basename="whats-the-weather">
-      <Router />
-    </BrowserRouter>
-  );
+  return <Router />;
 }
 
 export default App;
